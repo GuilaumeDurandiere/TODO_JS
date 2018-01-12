892caffb4a59eb406f0d0e511ae759e4d3b9897f
GuilaumeDurandiere <guillaume.durandiere@orange.fr>
2018-01-12 17:34:08 +0100

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,3 +1,5 @@
+window.onload = getTask();
+
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
@@ -14,7 +16,14 @@ var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
 	close[i].onclick = function() {
+		var txt = this.parentElement.innerText.substr(0, this.parentElement.innerText.length - 2);
 		var div = this.parentElement;
+
+		xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("POST","deleteTask.php",true);
+		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xmlhttp.send("task="+txt); 
+
 		div.style.display = "none";
 	}
 }
@@ -33,10 +42,17 @@ function newElement() {
 	var inputValue = document.getElementById("myInput").value;
 	var t = document.createTextNode(inputValue);
 	li.appendChild(t);
+
 	if (inputValue === '') {
-		alert("Vous devez renseigner quelque chose!");
+		alert("Champs vide !");
 	} else {
+
 		document.getElementById("myUL").appendChild(li);
+
+		xmlhttp = new XMLHttpRequest();
+		xmlhttp.open("POST","addTask.php",true);
+		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xmlhttp.send("task="+inputValue); 
 	}
 	document.getElementById("myInput").value = "";
 
@@ -48,8 +64,34 @@ function newElement() {
 
 	for (i = 0; i < close.length; i++) {
 		close[i].onclick = function() {
+
+			xmlhttp = new XMLHttpRequest();
+			xmlhttp.open("POST","deleteTask.php",true);
+			xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xmlhttp.send("task="+inputValue); 
+
 			var div = this.parentElement;
 			div.style.display = "none";
 		}
 	}
 }
+
+function getTask() {
+
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET","getTask.php",false);
+	xmlhttp.send();
+
+	json = JSON.parse(xmlhttp.responseText);
+
+	for (var i = json.length - 1; i >= 0; i--) {
+		var li = document.createElement("li");
+		var t = document.createTextNode(json[i][1]);
+		li.appendChild(t);
+		if(json[i][2] == "Oui"){
+			li.className = "checked";
+		}
+		document.getElementById("myUL").appendChild(li);
+	}
+}
+
